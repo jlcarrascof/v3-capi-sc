00d42232627d77d7e4a348420382b23ad942b8a4
Javier Martinez <entrenamientospuntonet@gmail.com>
2026-01-08 11:31:06 -0400

    refactor: remove SecondComponent import and usage from App.vue

--- class3-vue3-ts/src/App.vue
+++ class3-vue3-ts/src/App.vue
@@ -1,12 +1,10 @@
 <script setup lang="ts">
   import FirstComponent from './components/FirstComponent.vue'
-  import SecondComponent from './components/SecondComponent.vue'
 </script>
 
 <template>
   <div>
     <FirstComponent />
-    <SecondComponent />
   </div>
 </template>
 
